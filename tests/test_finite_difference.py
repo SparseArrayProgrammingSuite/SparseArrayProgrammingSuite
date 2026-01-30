251afab5c7fee138a2fcc40a13b4f402fabc7ec1
VMaster123 <73140174+VMaster123@users.noreply.github.com>
2026-01-30 10:59:23 -0500

    Draft Finite Difference

--- tests/test_finite_difference.py
+++ tests/test_finite_difference.py
@@ -3,6 +3,7 @@ import pytest
 import numpy as np
 
 from sparseappbench.benchmarks.Finite_Difference import (
+    buckley_leverett_flux,
     burgers_flux,
     lax_freidrichs_data_generator,
     lax_friedrichs_solver,
@@ -38,6 +39,51 @@ def test_lax_friedrichs_cfl_burgers(dt, dx, expect_stable):
 
     if expect_stable:
         assert np.isfinite(max_flux)
-        assert max_flux < 10.0
     else:
-        assert (not np.isfinite(max_flux)) or (max_flux > 1e2)
+        assert not np.isfinite(max_flux)
+
+
+def max_wave_speed_buckley(u):
+    eps = 1e-6
+    return np.max(
+        np.abs(
+            (buckley_leverett_flux(u + eps) - buckley_leverett_flux(u - eps))
+            / (2 * eps)
+        )
+    )
+
+
+# Now max for
+
+
+@pytest.mark.parametrize(
+    "dt, dx, expect_stable",
+    [
+        (0.01, 0.1, True),  # CFL << 1  → stable
+        (0.05, 0.1, True),  # CFL ≈ 0.5 → stable
+        (0.2, 0.1, False),  # CFL > 1   → unstable
+    ],
+)
+def test_lax_friedrichs_cfl_buckley_leverett_flux(dt, dx, expect_stable):
+    xp = NumpyFramework()
+    u_0 = lax_freidrichs_data_generator(xp, 200)
+    timesteps = 50
+    cmax = max_wave_speed_buckley(u_0)
+    fluxes_bench = lax_friedrichs_solver(
+        xp,
+        u_0,
+        dt=dt,
+        dx=dx,
+        flux=buckley_leverett_flux,
+        timesteps=timesteps,
+        boundary="dirichlet",
+    )
+
+    fluxes = xp.from_benchmark(fluxes_bench)
+
+    max_flux = np.max(np.abs(fluxes))
+
+    if cmax * dt / dx <= 1:
+        assert np.isfinite(max_flux)
+    else:
+        assert not np.isfinite(max_flux)
