694c6699d1c6c98bcf79b4ff283c1877ba9e0dcf
Dinofish32 <akarshd32@gmail.com>
2026-04-13 23:21:06 -0400

    first rk4 commit

--- tests/test_circuitsim.py
+++ tests/test_circuitsim.py
@@ -6,6 +6,7 @@ from scipy.integrate import solve_ivp
 from sparseappbench.benchmarks.circuitsim import (
     backward_euler,
     forward_euler,
+    rk4,
     lotka_volterra,
     rc,
     rlc,
@@ -60,6 +61,30 @@ def test_euler_forward(dydt, t_span, y0, step, tolerance, second_order):
 def test_euler_backward(dydt, t_span, y0, step, tolerance, second_order):
     """Test function for Backward Euler."""
     (time, y_euler) = backward_euler(np, dydt, t_span, y0, step)
+    y_euler = np.array(y_euler)
+
+    # Internally solve_ivp does not use fixed step sizes, unlike forward_euler
+    actual = solve_ivp(dydt, t_span, y0, t_eval=time)
+    print(y_euler)
+    print(actual.y.T)
+
+    if second_order:
+        error = np.max(np.abs(y_euler[:, 0] - actual.y[0].T))
+    else:
+        error = np.max(np.abs(y_euler - actual.y.T))
+    assert error < tolerance, f"Exceeds error tolerance: {error}"
+
+@pytest.mark.parametrize(
+    "dydt, t_span, y0, step, tolerance, second_order",
+    [
+        (dVdt_rc, (0, 5), [0], 0.000001, 0.05, False),
+        (dVdt_rlc, (0, 10e-3), [0, 0], 0.0000001, 0.05, True),
+        (dydt_lv, (0, 100), [40, 9], 0.00001, 0.5, False),
+    ],
+)
+def test_rk4(dydt, t_span, y0, step, tolerance, second_order):
+    """Test function for Runge-Kutta 4th order."""
+    (time, y_euler) = rk4(np, dydt, t_span, y0, step)
     y_euler = np.array(y_euler)
 
     # Internally solve_ivp does not use fixed step sizes, unlike forward_euler
