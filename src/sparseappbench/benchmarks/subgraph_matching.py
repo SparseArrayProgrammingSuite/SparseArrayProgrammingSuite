15083b60c9ae500a86b5ff23d7de4b4bde8f4ddb
neflow <20128868+nestflow@users.noreply.github.com>
2026-04-15 21:29:17 -0400

    Finish migration (new workflow and ways to construct metadata)

--- src/sparseappbench/benchmarks/subgraph_matching.py
+++ src/sparseappbench/benchmarks/subgraph_matching.py
@@ -1,80 +1,65 @@
-"""
-Name: Subgraph Matching
-Author: Taishan Chen
-Email: utallow@bu.edu
-
-Motivation:
-Subgraph matching is a classic problem and widely used in query evaluations in databases. 
-
-Role of sparsity:
-The graph structures from databases are usually much sparse, 
-and naive approaches with adjacent matrices suffers from O(V^2) space requirements.
-
-Implementation:
-This code is hand written by me with help and discussion with @kylebd99.
-
-Dataset:
-The G-CARE dataset is provided by https://github.com/yspark-dblab/gcare.
-This code will download the data files from the Google Drive links they provide.
-
-Generative AI:
-No generative AI was used to implement benchmark functions.
-"""
-
-import gdown
-from pathlib import Path
-import os
-import tarfile
 import numpy as np
-from sparseappbench.binsparse_format import BinsparseFormat
+from pathlib import Path
+import tarfile
+import gdown
+
+import sparseappbench
+from sparseappbench.benchmark import (
+    Author,
+    Benchmark,
+    BinsparseFormat,
+    Contributor,
+    Dataset,
+    Generator,
+    Ref,
+)
+
+xp = sparseappbench.xp
 
 
-def read_gcare_query(p: Path, continous_label=True):
-    with p.open('r', encoding='utf-8') as f:
-        exprs = []
-        qvs = []
-        sp_mats_name = set()
-        for line in f.readlines():
-            if line.startswith('t'):
-                query_id = int(line.strip().split(' ')[-1])
-            elif line.startswith('v'):
-                vals = line.strip().split(' ')
-                qv_id = int(vals[1])
-                v_label = int(vals[2])
-                v_id = int(vals[3])
-                qvs.append(f'v_{qv_id}')
-                if v_label == -1:
-                    # Since all vertices have at least one label then it's unnecessary
-                    # exprs.append(f'VA[v_{qv_id}]')
-                    # sp_mats_name.add(f'VA')
-                    pass
-                else:
-                    # Q[qv_id] must have label of v_label
-                    exprs.append(f'V{v_label}[v_{qv_id}]')
-                    sp_mats_name.add(f'V{v_label}')
+def download_gcare_data(root_dir: Path):
+    dataset_dir = root_dir / 'dataset'
+    queryset_dir = root_dir / 'queryset'
+    ground_truth_dir = root_dir / 'ground_truth'
 
-                if v_id == -1:
-                    # C should be a vector that for all v_id existed, C[v_id] = 1 and all else 0
-                    # i.e C = union of all U_{v_id}
-                    # If v_id is continous: [0, 1, ..., max_vid] then it's unnecessary
-                    if not continous_label:
-                        exprs.append(f'C[v_{qv_id}]')
-                        sp_mats_name.add(f'C')
-                else:
-                    # QV[qv_id] must have index of v_id
-                    exprs.append(f'P{v_id}[v_{qv_id}]')
-                    sp_mats_name.add(f'P{v_id}')
-                    # P_{v_id} is a unit (one-hot vector) where P[v_id] = 1
-            elif line.startswith('e'):
-                vals = line.strip().split(' ')
-                qv_id1 = int(vals[1])
-                qv_id2 = int(vals[2])
-                e_label = int(vals[3])
-                exprs.append(f'E{e_label}[v_{qv_id1},v_{qv_id2}]')
-                sp_mats_name.add(f'E{e_label}')
+    dataset_link = 'https://drive.google.com/file/d/1HAgSVE-24NOap6_Q1_twH56Dkb2kPvGU/view?usp=sharing'
+    queryset_link = 'https://drive.google.com/file/d/1Dlj43rBAOVPAsfzKlYxIbZ9RsqeGM_MN/view?usp=sharing'
+    ground_truth_link = 'https://drive.google.com/file/d/1Bc6Q2RZQTcIB8IfOw5KafNYwPhq2BO94/view?usp=sharing'
 
-        final_expr = 'S[] += ' + ' * '.join(exprs)
-        return final_expr, qvs, sp_mats_name
+    # Download dataset
+    if not dataset_dir.exists():
+        dataset_dir.mkdir(parents=True, exist_ok=True)
+
+    gdown.cached_download(dataset_link,
+                          str(dataset_dir / 'dataset.tar.gz'),
+                          hash='sha256:78B86CDA06115C4554CDFCFB93A7FBC8ECB759DF39927510DD02CED4228A95E4'.lower())
+
+    with tarfile.open(dataset_dir / 'dataset.tar.gz', 'r:gz') as tar:
+        tar.extractall(path=dataset_dir, filter='data')
+
+    # Download queryset
+    if not queryset_dir.exists():
+        queryset_dir.mkdir(parents=True, exist_ok=True)
+
+    gdown.cached_download(queryset_link,
+                          str(queryset_dir / 'queryset.tar.gz'),
+                          hash='sha256:C8DC9F978296559E9E55335A989CE16E7B5BCBA7AA9D43E25FBD9E588D00EBC7'.lower())
+
+    with tarfile.open(queryset_dir / 'queryset.tar.gz', 'r:gz') as tar:
+        tar.extractall(path=queryset_dir, filter='data')
+
+    # Download true cardinality
+    if not ground_truth_dir.exists():
+        ground_truth_dir.mkdir(parents=True, exist_ok=True)
+
+    gdown.cached_download(ground_truth_link,
+                          str(ground_truth_dir / 'ground_truth.tar.gz'),
+                          hash='sha256:22E59F4FC06FFB79711D582513C6422CA555422C9947FDE64A52F0A9292D382C'.lower())
+
+    with tarfile.open(ground_truth_dir / 'ground_truth.tar.gz', 'r:gz') as tar:
+        tar.extractall(path=ground_truth_dir, filter='data')
+
+    return (dataset_dir, queryset_dir, ground_truth_dir)
 
 
 def read_gcare_data(p: Path):
@@ -172,69 +157,52 @@ def read_gcare_data(p: Path):
         return max_vid, continous_label, sp_mats
 
 
-def run_one_match(xp, sp_mats: dict, expr: str):
-    for key, val in sp_mats.items():
-        sp_mats[key] = xp.from_benchmark(val)
-    count = xp.einsum(expr, **sp_mats)
-    return xp.to_benchmark(np.array(count))
+def read_gcare_query(p: Path, continous_label=True):
+    with p.open('r', encoding='utf-8') as f:
+        exprs = []
+        qvs = []
+        sp_mats_name = set()
+        for line in f.readlines():
+            if line.startswith('t'):
+                query_id = int(line.strip().split(' ')[-1])
+            elif line.startswith('v'):
+                vals = line.strip().split(' ')
+                qv_id = int(vals[1])
+                v_label = int(vals[2])
+                v_id = int(vals[3])
+                qvs.append(f'v_{qv_id}')
+                if v_label == -1:
+                    # Since all vertices have at least one label then it's unnecessary
+                    # exprs.append(f'VA[v_{qv_id}]')
+                    # sp_mats_name.add(f'VA')
+                    pass
+                else:
+                    # Q[qv_id] must have label of v_label
+                    exprs.append(f'V{v_label}[v_{qv_id}]')
+                    sp_mats_name.add(f'V{v_label}')
 
+                if v_id == -1:
+                    # C should be a vector that for all v_id existed, C[v_id] = 1 and all else 0
+                    # i.e C = union of all U_{v_id}
+                    # If v_id is continous: [0, 1, ..., max_vid] then it's unnecessary
+                    if not continous_label:
+                        exprs.append(f'C[v_{qv_id}]')
+                        sp_mats_name.add(f'C')
+                else:
+                    # QV[qv_id] must have index of v_id
+                    exprs.append(f'P{v_id}[v_{qv_id}]')
+                    sp_mats_name.add(f'P{v_id}')
+                    # P_{v_id} is a unit (one-hot vector) where P[v_id] = 1
+            elif line.startswith('e'):
+                vals = line.strip().split(' ')
+                qv_id1 = int(vals[1])
+                qv_id2 = int(vals[2])
+                e_label = int(vals[3])
+                exprs.append(f'E{e_label}[v_{qv_id1},v_{qv_id2}]')
+                sp_mats_name.add(f'E{e_label}')
 
-def benchmark_subgraph_matching(xp, queries):
-    counts = np.zeros((len(queries), ), dtype=np.int64)
-    for i, (sp_mats, expr) in enumerate(queries):
-        for key, val in sp_mats.items():
-            sp_mats[key] = xp.from_benchmark(val)
-        counts[i] = xp.einsum(expr, **sp_mats)
-    return xp.to_benchmark(counts)
-
-
-def download_gcare_data():
-    root_dir = Path('./data/gcare')
-    dataset_dir = root_dir / 'dataset'
-    queryset_dir = root_dir / 'queryset'
-    ground_truth_dir = root_dir / 'ground_truth'
-
-    dataset_link = 'https://drive.google.com/file/d/1HAgSVE-24NOap6_Q1_twH56Dkb2kPvGU/view?usp=sharing'
-    queryset_link = 'https://drive.google.com/file/d/1Dlj43rBAOVPAsfzKlYxIbZ9RsqeGM_MN/view?usp=sharing'
-    grount_truth_link = 'https://drive.google.com/file/d/1Bc6Q2RZQTcIB8IfOw5KafNYwPhq2BO94/view?usp=sharing'
-
-    # Download dataset
-    if not dataset_dir.exists():
-        os.makedirs(dataset_dir, exist_ok=True)
-
-    gdown.cached_download(dataset_link,
-                          str(dataset_dir / 'dataset.tar.gz'),
-                          hash='sha256:78B86CDA06115C4554CDFCFB93A7FBC8ECB759DF39927510DD02CED4228A95E4'.lower(),
-                          fuzzy=True)
-
-    with tarfile.open(dataset_dir / 'dataset.tar.gz', 'r:gz') as tar:
-        tar.extractall(path=dataset_dir, filter='data')
-
-    # Download queryset
-    if not queryset_dir.exists():
-        os.makedirs(dataset_dir, exist_ok=True)
-
-    gdown.cached_download(queryset_link,
-                          str(queryset_dir / 'queryset.tar.gz'),
-                          hash='sha256:C8DC9F978296559E9E55335A989CE16E7B5BCBA7AA9D43E25FBD9E588D00EBC7'.lower(),
-                          fuzzy=True)
-
-    with tarfile.open(queryset_dir / 'queryset.tar.gz', 'r:gz') as tar:
-        tar.extractall(path=queryset_dir, filter='data')
-
-    # Download true cardinality
-    if not ground_truth_dir.exists():
-        os.makedirs(ground_truth_dir, exist_ok=True)
-
-    gdown.cached_download(grount_truth_link,
-                          str(ground_truth_dir / 'ground_truth.tar.gz'),
-                          hash='sha256:22E59F4FC06FFB79711D582513C6422CA555422C9947FDE64A52F0A9292D382C'.lower(),
-                          fuzzy=True)
-
-    with tarfile.open(ground_truth_dir / 'ground_truth.tar.gz', 'r:gz') as tar:
-        tar.extractall(path=ground_truth_dir, filter='data')
-
-    return (dataset_dir, queryset_dir)
+        final_expr = 'S[] += ' + ' * '.join(exprs)
+        return final_expr, qvs, sp_mats_name
 
 
 def process_one_query(query_path, all_sp_mats, max_vid, continous_label):
@@ -278,38 +246,220 @@ def process_one_query(query_path, all_sp_mats, max_vid, continous_label):
     return (sp_mats_needed, expr)
 
 
-def process_queries(queryset_dir, all_sp_mats, max_vid, continous_label):
-    ret_val = []
-    for query_path in queryset_dir.rglob('*.txt'):
-        ret_val.append(process_one_query(
-            query_path, all_sp_mats, max_vid, continous_label))
+class SubgraphGCareDataset(Dataset):
+    def __init__(
+        self,
+        name,
+        pretty_name,
+        description,
+        tags,
+    ):
+        self._name = name
+        self._pretty_name = pretty_name
+        self._description = description
+        self._tags = tags
 
-    return ret_val
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def pretty_name(self) -> str:
+        return self._pretty_name
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+    @property
+    def tags(self) -> list[str]:
+        return self._tags
 
 
-def gcare_human_all():
-    (dataset_dir, queryset_dir) = download_gcare_data()
-    (max_vid, continous_label, all_sp_mats) = read_gcare_data(
-        dataset_dir / 'human' / 'human.txt')
-    return process_queries(queryset_dir / 'human', all_sp_mats, max_vid, continous_label)
+class SubgraphGCareGenerator(Generator[SubgraphGCareDataset]):
+    @property
+    def name(self) -> str:
+        return "subgraph_gcare_inputs"
+
+    @property
+    def pretty_name(self) -> str:
+        return "Subgraph G-CARE Input Generator"
+
+    @property
+    def description(self) -> str:
+        return (
+            "Transforms the G-CARE dataset to the input of subgraph matching algorithms. "
+        )
+
+    @property
+    def tags(self) -> list[str]:
+        return ["subgraph matching", "sparse"]
+
+    @property
+    def authors(self) -> list[Contributor]:
+        return [
+            Contributor("Taishan Chen", "utallow@bu.edu"),
+            Contributor("Kyle Deeds", "kdeeds@bu.edu"),
+        ]
+
+    @property
+    def references(self) -> list[Ref]:
+        return [
+            Ref(
+                title=(
+                    "G-CARE: "
+                    "A Framework for Performance Benchmarking of Cardinality Estimation Techniques "
+                    "for Subgraph Matching "
+                ),
+                authors=[
+                    Author("Yeonsu Park"),
+                    Author("Seongyun Ko"),
+                    Author("Sourav S Bhowmick"),
+                    Author("Kyoungmin Kim"),
+                    Author("Kijae Hong"),
+                    Author("Wook-Shin Han"),
+                ],
+                year=2020,
+                url="https://dl.acm.org/doi/10.1145/3318464.3389702",
+            ),
+        ]
+
+    @property
+    def ai_disclosure(self) -> str:
+        return (
+            "No generative AI was used to write the algorithms for the benchmark function. "
+            "Generative AI might have been used to construct the definition of the framework."
+        )
+
+    @property
+    def motivation(self) -> str:
+        return (
+            "Subgraph matching and counting are classic problems and widely used in query evaluations in database systems. "
+        )
+
+    @property
+    def datasets(self) -> list[SubgraphGCareDataset]:
+        return [
+            SubgraphGCareDataset(
+                name="human",
+                pretty_name="G-CARE Human Subset (Small)",
+                description=(
+                    "G-CARE Human Subset (Small)"
+                ),
+                tags=["small", "sparse"],
+            ),
+            SubgraphGCareDataset(
+                name="aids",
+                pretty_name="G-CARE AIDS Subset (Medium)",
+                description=(
+                    "G-CARE AIDS Subset (Medium)"
+                ),
+                tags=["medium", "sparse"],
+            ),
+            SubgraphGCareDataset(
+                name="lubm80",
+                pretty_name="G-CARE LUBM80 Subset (Large)",
+                description=(
+                    "G-CARE LUBM80 Subset (Large)"
+                ),
+                tags=["large", "sparse"],
+            ),
+            SubgraphGCareDataset(
+                name="yago",
+                pretty_name="G-CARE YAGO Subset (Huge)",
+                description=(
+                    "G-CARE YAGO Subset (Huge)"
+                ),
+                tags=["huge", "sparse"],
+            ),
+        ]
+
+    def generate(self, dataset: SubgraphGCareDataset):
+        root_dir = Path('./data/gcare')
+        (dataset_dir, queryset_dir, ground_truth_dir) = download_gcare_data(root_dir)
+        (max_vid, continous_label, all_sp_mats) = read_gcare_data(
+            dataset_dir / dataset.name / f'{dataset.name}.txt')
+
+        queries = {}
+
+        for query_path in (queryset_dir / dataset.name).rglob('*.txt'):
+            (sp_mats_needed, expr) = process_one_query(
+                query_path, all_sp_mats, max_vid, continous_label)
+            queries[query_path.stem] = {
+                "matrices": sp_mats_needed, "expr": expr}
+
+        for gt_path in (ground_truth_dir / dataset.name).rglob('*.txt'):
+            with open(gt_path, 'r') as f:
+                queries[gt_path.stem]["ground_truth"] = int(f.readline())
+
+        matrices = []
+        meta = {'exprs': [], 'gts': [], 'names': []}
+        for query_name, query_data in queries.items():
+            matrices.append(query_data["matrices"])
+            meta['exprs'].append(query_data["expr"])
+            meta['gts'].append(query_data["ground_truth"])
+            meta['names'].append(query_name)
+
+        return matrices, meta
 
 
-def gcare_human_one(query_name: str):
-    (dataset_dir, queryset_dir) = download_gcare_data()
-    (max_vid, continous_label, all_sp_mats) = read_gcare_data(
-        dataset_dir / 'human' / 'human.txt')
-    
-    query_path = queryset_dir / query_name
-    if not query_path.exists():
-        raise Exception(f'Query {query_name} does not exist! Path {query_path}')
+class SubgraphMatching(Benchmark):
+    @property
+    def tag(self):
+        return "subgraph_matching"
 
-    return process_one_query(query_path, all_sp_mats, max_vid, continous_label)
+    @property
+    def name(self):
+        return "Subgraph Matching Algorithm using einsum"
 
+    @property
+    def pretty_name(self):
+        return "Subgraph Matching Algorithm using einsum"
 
-# Commented because current framework supported cannot handle this large
+    @property
+    def description(self):
+        return (
+            "Benchmarks subgraph matching algorithms using einsum operations."
+        )
 
-# def gcare_aids():
-#     (dataset_dir, queryset_dir) = download_gcare_data()
-#     (max_vid, continous_label, all_sp_mats) = read_gcare_data(
-#         dataset_dir / 'aids' / 'aids.txt')
-#     return process_queries(queryset_dir / 'aids', all_sp_mats, max_vid, continous_label)
+    @property
+    def tags(self):
+        return ["subgraph-matching", "sparse"]
+
+    @property
+    def authors(self):
+        return [Contributor("Taishan Chen", "utallow@bu.edu"),
+                Contributor("Kyle Deeds", "kdeeds@bu.edu")]
+
+    @property
+    def references(self):
+        return []
+
+    @property
+    def ai_disclosure(self):
+        return (
+            "No generative AI was used to write the algorithms for the benchmark function. "
+            "Generative AI might have been used to construct the definition of the framework."
+        )
+
+    @property
+    def motivation(self):
+        return (
+            "Subgraph matching and counting are classic problems and widely used in query evaluations in database systems. "
+        )
+
+    @property
+    def generators(self):
+        return [SubgraphGCareGenerator()]
+
+    def benchmark(self, data, meta):
+        exprs = meta['exprs']
+        assert len(data) == len(exprs)
+        counts = np.zeros((len(data), ), dtype=np.int64)
+        for i in range(len(data)):
+            sp_mats = data[i]
+            expr = exprs[i]
+            for key, val in sp_mats.items():
+                sp_mats[key] = xp.from_binsparse(val)
+            counts[i] = xp.einsum(expr, **sp_mats)
+        return xp.to_binsparse(counts)
--- tests/test_subgraph_matching.py
+++ tests/test_subgraph_matching.py
@@ -1,30 +1,23 @@
-from pathlib import Path
-
-from sparseappbench.benchmarks.subgraph_matching import benchmark_subgraph_matching, gcare_human_all
+import sparseappbench.benchmarks.subgraph_matching as subgraph_matching
 from sparseappbench.frameworks.numpy_framework import NumpyFramework
 from sparseappbench.frameworks.sparse_framework import PyDataSparseFramework
-from sparseappbench.binsparse_format import BinsparseFormat
 
-def test_human_all():
-    queries = gcare_human_all()
+
+def test_human_subset():
     xp = PyDataSparseFramework()
-    results = benchmark_subgraph_matching(xp, queries)
-    results = xp.from_benchmark(results)
+    subgraph_matching.xp = xp
+    dataset = subgraph_matching.SubgraphGCareGenerator().datasets[0]
+    (matrices, meta) = subgraph_matching.SubgraphGCareGenerator().generate(dataset)
+    # print(f'Meta: {meta}')
 
-    ground_truth_dir = Path('./data/gcare/ground_truth') / 'human'
-
-    gts = []
-    test_names = []
-    for gt_file in ground_truth_dir.rglob('*.txt'):
-        with open(gt_file, 'r') as f:
-            test_names.append(f'{gt_file.parent}, {gt_file.stem}')
-            gts.append(int(f.readline()))
+    results = subgraph_matching.SubgraphMatching().benchmark(matrices, meta)
+    results = xp.from_binsparse(results)
 
     for i in range(len(results)):
         res = results[i]
-        gt = gts[i]
-        assert res == gt, f'Test {test_names[i]} incorrect: Result = {res}, Ground Truth = {gt}'
+        gt = meta['gts'][i]
+        # print(f'Test {meta["names"][i]}: Result = {res}, Ground Truth = {gt}')
+        assert res == gt, f'Test {meta["names"][i]} incorrect: Result = {res}, Ground Truth = {gt}'
 
-
-if __name__ == '__main__':
-    test_human_all()
+if __name__ == "__main__":
+    test_human_subset()
