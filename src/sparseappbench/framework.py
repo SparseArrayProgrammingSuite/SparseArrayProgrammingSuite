38e4925848881d7a5d186540a1b4f51190c5586f
Willow Ahrens <ahrens@gatech.edu>
2026-04-24 16:28:10 -0400

    fix

--- src/sparseappbench/framework.py
+++ src/sparseappbench/framework.py
@@ -7,7 +7,9 @@ if framework_path is not None:
     import importlib.util
 
     spec = importlib.util.spec_from_file_location("custom_framework", framework_path)
+    assert spec is not None, "Failed to create module spec"
     custom_framework = importlib.util.module_from_spec(spec)
+    assert spec.loader is not None, "Module spec has no loader"
     spec.loader.exec_module(custom_framework)
     xp = custom_framework.xp
     assert isinstance(xp, Framework), (
