4ba90eb409d3ac3a10c3002a9c6d05319548b92f
Richard Wan <myrst131@gmail.com>
2026-04-24 13:59:15 -0400

    updated to new flow

---
+++ tests/test_model_counting.py
@@ -0,0 +1,24 @@
+import sparseappbench.benchmarks.model_counting as mc
+from sparseappbench.frameworks.numpy_framework import NumpyFramework
+
+
+def test_model_counting_datasets():
+    xp = NumpyFramework()
+    mc.xp = xp
+
+    generator = mc.MCGenerator()
+    benchmark = mc.ModelCounting()
+
+    for dataset in generator.datasets:
+        raw_matrices, meta = generator.generate(dataset)
+
+        input_arrays = [xp.from_binsparse(m) for m in raw_matrices]
+
+        results = benchmark.benchmark(input_arrays, meta)
+
+        res = int(results[0])
+        expected = meta["expected_result"]
+
+        msg = f"Test '{dataset.name}' failed: Expected {expected}, got {res}"
+
+        assert res == expected, msg
