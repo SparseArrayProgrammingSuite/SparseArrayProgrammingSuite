e029b06a023277847c248dc3322e7405156bb978
Willow Ahrens <willow.marie.ahrens@gmail.com>
2026-07-01 17:23:34 -0400

    here goes

--- src/saps/utils/scrape_matrices.py
+++ src/saps/utils/scrape_matrices.py
@@ -290,10 +290,10 @@ def main():
             )
 
 
-def calculate_and_save_solver_result(output_file, matrix, A, m, n, solver, tol=1e-6):
+def calculate_and_save_solver_result(output_file, matrix, A, m, n, solver, tol=1e-3):
     threshold = convergence_threshold(solver)
 
-    for tol in [1e-2, tol]:
+    for tol in [0.05, tol]:
         try:
             convergence_value = SOLVER_DICT[solver](A, tol=tol)
         except (ArpackError, RuntimeError, ValueError, np.linalg.LinAlgError) as e:
