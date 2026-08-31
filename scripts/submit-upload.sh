ecc91a169f03fd1862f928288b8f902b205b64c8
Willow Ahrens <willow@csail.mit.edu>
2026-08-31 17:48:18 -0400

    amd

--- scripts/submit-upload.sh
+++ scripts/submit-upload.sh
@@ -8,4 +8,4 @@ if ! aws configure list-profiles | grep -Fxq dataset-upload; then
 fi
 
 aws sso login --profile dataset-upload --use-device-code
-sbatch -A gts-wahrens6 "$script_directory/upload-dataset.slurm" "$1"
+sbatch -A gts-wahrens6 -q embers -C amd "$script_directory/upload-dataset.slurm" "$1"
