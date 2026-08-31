8eb1003fc7dc2f54d356f32a88cd72d56b83dc6a
Willow Ahrens <willow@csail.mit.edu>
2026-08-31 17:57:32 -0400

    fix

--- scripts/submit-upload.sh
+++ scripts/submit-upload.sh
@@ -2,10 +2,14 @@
 set -e
 
 script_directory=$(cd -- "$(dirname -- "$0")" && pwd)
+repo_directory=$(cd -- "$script_directory/.." && pwd)
+
+export PATH="$HOME/.local/bin:$PATH"
 
 if ! aws configure list-profiles | grep -Fxq dataset-upload; then
   aws configure sso --profile dataset-upload --use-device-code
 fi
 
 aws sso login --profile dataset-upload --use-device-code
-sbatch -A gts-wahrens6 -q embers -C amd "$script_directory/upload-dataset.slurm" "$1"
+cd "$repo_directory"
+sbatch -A gts-wahrens6 -q embers -C amd "$script_directory/upload-dataset.slurm"
